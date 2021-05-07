b1433e59cc250fa8751aa8c2661167dfc1d86bcf
Jay Li <j_li@g.harvard.edu>
2021-05-06 21:29:24 -0400

    up to Q17

--- rbf.py
+++ rbf.py
@@ -2,7 +2,7 @@ import numpy as np
 from qpsolvers import solve_qp
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-from hw8.libsvm.python.svmutil import *
+from hw8.libsvm.python.libsvm.svmutil import *
 
 
 class Target:
@@ -140,6 +140,76 @@ class RadialBasisSVM:
         return y.reshape((-1, 1))
 
 
+class RegularRBF:
+    def __init__(self, K, gamma):
+        self.K = K
+        self.gamma = gamma
+
+        self.centers = None
+        self.w = None
+
+        self.rng = np.random.default_rng()
+
+    def k_means(self, X):
+        """ Compute K centers in the dataset
+        :param X: shape (N, d)
+        :return: shape (K, d)
+        """
+        N, d = X.shape
+
+        centers = self.rng.choice(X, size=self.K, replace=False, shuffle=False)
+        assert centers.shape == (self.K, d)
+
+        while True:
+            sets = [[] for _ in range(self.K)]
+            for x in X:
+                best_center_idx = None
+                lowest_dist = None
+                for idx, center in enumerate(centers):
+                    dist = np.linalg.norm(x-center)
+                    if best_center_idx is None:
+                        lowest_dist = dist
+                        best_center_idx = idx
+                    elif dist < lowest_dist:
+                        lowest_dist = dist
+                        best_center_idx = idx
+                sets[best_center_idx].append(x)
+            new_centers = np.array([np.mean(s, axis=0) for s in sets])
+            if np.array_equal(new_centers, centers):
+                return centers
+            centers = new_centers
+
+    def fit(self, X, y):
+        centers = self.k_means(X)
+
+        N = X.shape[0]
+
+        Z = np.ones((N, self.K+1))
+        for i, x in enumerate(X):
+            for j, mu in enumerate(centers):
+                x_diff = x - mu
+                Z[i, j+1] = np.exp(-self.gamma * np.inner(x_diff, x_diff))
+
+        w = np.linalg.lstsq(Z, y, rcond=None)[0]
+
+        self.centers = centers
+        self.w = w
+
+    def compute_y(self, X):
+        N = X.shape[0]
+
+        Z = np.ones((N, self.K+1))
+        for i, x in enumerate(X):
+            for j, mu in enumerate(self.centers):
+                x_diff = x - mu
+                Z[i, j+1] = np.exp(-self.gamma * np.inner(x_diff, x_diff))
+
+        y = np.matmul(Z, self.w.reshape((-1, 1)))
+        y[y > 0] = 1
+        y[y <= 0] = -1
+        return y
+
+
 def error_rate(y1, y2):
     N = y1.shape[0]
     assert y1.shape == y2.shape == (N, 1)
@@ -147,7 +217,7 @@ def error_rate(y1, y2):
 
 
 if __name__ == "__main__":
-    experiment = 13
+    experiment = 17
     if experiment == 13:
         N = 100
         gamma = 1.5
@@ -168,4 +238,116 @@ if __name__ == "__main__":
         percentage = not_sep_count / N_exp * 100
         print("percentage of time that is not separable: {}%".format(percentage))
         print("mean E_in: {}%".format(np.mean(E_ins)))
+    elif experiment == 14 or experiment == 15:
+        K = 12
+        gamma = 1.5
+        N_exp = 200
+        N = 100
+        svm_win = []
+        for _ in tqdm(range(N_exp)):
+            X, y = Target().generate(N)
+            X_test, y_test = Target().generate(1000)
 
+            rbf = RegularRBF(K=K, gamma=gamma)
+            rbf.fit(X, y)
+            y_hat_rbf = rbf.compute_y(X_test)
+            E_out_rbf = error_rate(y_hat_rbf, y_test)
+
+            svm = RadialBasisSVM(gamma=gamma, libsvm=True)
+            svm.fit(X, y)
+            y_hat_svm = svm.compute_y(X_test)
+            E_out_svm = error_rate(y_hat_svm, y_test)
+
+            svm_win.append(E_out_svm < E_out_rbf)
+
+        print("Percentage of time that SVM wins: {}%".format(np.sum(svm_win) / N_exp * 100))
+    elif experiment == 16:
+        gamma = 1.5
+        a_counts = 0
+        b_counts = 0
+        c_counts = 0
+        d_counts = 0
+        e_counts = 0
+        N_exp = 200
+        N = 100
+        for _ in tqdm(range(N_exp)):
+            X, y = Target().generate(N)
+            X_test, y_test = Target().generate(1000)
+
+            # K = 9
+            rbf = RegularRBF(K=9, gamma=gamma)
+            rbf.fit(X, y)
+
+            y_hat = rbf.compute_y(X)
+            E_in_9 = error_rate(y_hat, y)
+
+            y_hat = rbf.compute_y(X_test)
+            E_out_9 = error_rate(y_hat, y_test)
+
+            # K = 12
+            rbf = RegularRBF(K=12, gamma=gamma)
+            rbf.fit(X, y)
+
+            y_hat = rbf.compute_y(X)
+            E_in_12 = error_rate(y_hat, y)
+
+            y_hat = rbf.compute_y(X_test)
+            E_out_12 = error_rate(y_hat, y_test)
+
+            # Compute results
+            if E_in_12 < E_in_9 and E_out_12 > E_out_9:
+                a_counts += 1
+            elif E_in_12 > E_in_9 and E_out_12 < E_out_9:
+                b_counts += 1
+            elif E_in_12 > E_in_9 and E_out_12 > E_out_9:
+                c_counts += 1
+            elif E_in_12 < E_in_9 and E_out_12 < E_out_9:
+                d_counts += 1
+            elif E_in_12 == E_in_9 and E_out_12 == E_out_9:
+                e_counts += 1
+        print("a: {}, b: {}, c: {}, d: {}, e: {}".format(a_counts, b_counts, c_counts, d_counts, e_counts))
+    elif experiment == 17:
+        K = 9
+        a_counts = 0
+        b_counts = 0
+        c_counts = 0
+        d_counts = 0
+        e_counts = 0
+        N_exp = 200
+        N = 100
+        for _ in tqdm(range(N_exp)):
+            X, y = Target().generate(N)
+            X_test, y_test = Target().generate(1000)
+
+            # gamma = 1.5
+            rbf = RegularRBF(K=K, gamma=1.5)
+            rbf.fit(X, y)
+
+            y_hat = rbf.compute_y(X)
+            E_in_9 = error_rate(y_hat, y)
+
+            y_hat = rbf.compute_y(X_test)
+            E_out_9 = error_rate(y_hat, y_test)
+
+            # gamma = 2
+            rbf = RegularRBF(K=K, gamma=2)
+            rbf.fit(X, y)
+
+            y_hat = rbf.compute_y(X)
+            E_in_12 = error_rate(y_hat, y)
+
+            y_hat = rbf.compute_y(X_test)
+            E_out_12 = error_rate(y_hat, y_test)
+
+            # Compute results
+            if E_in_12 < E_in_9 and E_out_12 > E_out_9:
+                a_counts += 1
+            elif E_in_12 > E_in_9 and E_out_12 < E_out_9:
+                b_counts += 1
+            elif E_in_12 > E_in_9 and E_out_12 > E_out_9:
+                c_counts += 1
+            elif E_in_12 < E_in_9 and E_out_12 < E_out_9:
+                d_counts += 1
+            elif E_in_12 == E_in_9 and E_out_12 == E_out_9:
+                e_counts += 1
+        print("a: {}, b: {}, c: {}, d: {}, e: {}".format(a_counts, b_counts, c_counts, d_counts, e_counts))
